aca6c80a5dd8d41c59921f6b1942d492e36c4eae
Steven Xu <stevenxu@live.ca>
2016-06-21 20:32:16 -0400

    Improve header animation with fade instead of background change

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,20 @@
 $(document).foundation();
+
+var header = $(".header-name");
+$('.top-bar-container').on('sticky.zf.stuckto:top', function(){
+	if(header.is(":visible")) {
+		MotionUI.animateOut(header, 'fade-out');
+	} else {
+		header.one("transitionend", function(){
+			MotionUI.animateOut(header, 'fade-out');
+		});
+	}
+}).on('sticky.zf.unstuckfrom:top', function(){
+	if(header.is(":visible")) {
+		header.one("transitionend", function(){
+			MotionUI.animateIn(".header-name", 'fade-in');
+		});
+	} else {
+		MotionUI.animateIn(".header-name", 'fade-in');
+	}
+})
