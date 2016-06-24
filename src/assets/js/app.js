5173ccd6987c37d369410a57c93af6ae02c7e5ec
Steven Xu <stevenxu@live.ca>
2016-06-24 14:31:08 -0400

    Add caption code for gallery

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -20,10 +20,32 @@ $('.top-bar-container').on('sticky.zf.stuckto:top', function(){
 });
 
 // Initialize gallery
-$('.gallery').featherlightGallery({
+var gallery = $('.gallery').featherlightGallery({
     openSpeed: 300,
 		previousIcon: '&#12296;',
-		nextIcon: '&#12297;'
+		nextIcon: '&#12297;',
+		beforeContent: function(){
+			// remove any existing captions and add the caption
+			// for the current slide
+			var numExistingCaptions = 
+				this.$instance.find(".caption").length;
+			var caption = this.$currentTarget
+				.find(".caption")
+				.clone();
+			caption.hide();
+			this.$instance
+				.children(".featherlight-content")
+				.append(caption);
+
+			this.$instance
+				.find(".caption")
+				.fadeOut(this.closeSpeed);
+			if (numExistingCaptions > 0) {
+				caption.delay(this.closeSpeed).fadeIn(this.openSpeed);
+			} else {
+				caption.fadeIn(this.openSpeed);
+			}
+		}
 });
 // Create a stackup object.
 var stackup = new StackUp({
